d057693d506e3f52d678fbaf7bb340ee7dfe9504
macm <mac.mitsch@comcast.net>
2022-07-05 01:35:15 -0500

    finished employee and engineer

--- Lib/__test__/employee.test.js
+++ Lib/__test__/employee.test.js
@@ -1,8 +1,9 @@
+
 const Employee = require("../employee");
 
 test("Can represent and instance", function () {
     const emp = new Employee();
-    expect(typeof (emp)).toBe('object');
+    expect(typeof(emp)).toBe('object');
 });
 // Set Name with Constructor
 test("Set name", function() {
@@ -17,26 +18,34 @@ test("set id", function () {
     expect(emp.id).toBe(testValue);
 });
 // Set Email with Constructor
-test("set Email", function (){
-    const testValue ="mystcoding@gmail.com";
+test("set Email", function () {
+    const testValue ="myst@test.com";
     const emp = new Employee("tony",1,testValue);
     expect(emp.email).toBe(testValue);
 });
+
 // Get Name W/ getName()
-test("get name from command", function(){
+test("get name from command", function() {
     const testValue = "tony";
     const emp = new Employee(testValue);
     expect(emp.getName()).toBe(testValue);
 });
 // Get Id W/ getId()
-test("get name from command", function(){
+test("get name from command", function() {
     const testValue = 100;
     const emp = new Employee("tony",testValue);
     expect(emp.getId()).toBe(testValue);
 });
 // Get Email W/ getEmail()
 test("get email from command", function () {
-    const testValue = "mystcoding@gmail.com";
+    const testValue = "myst@test.com";
     const emp = new Employee("tony",1,testValue);
     expect(emp.getEmail()).toBe(testValue);
 }); 
+// Retrieving  employee role
+// issues working due to getRole
+test("role returning employee", function () {
+    const testValue = "Employee";
+    const emp = new Employee("tony",1,"myst@test.com", "Employee");
+    expect(emp.getRole()).toBe(testValue);
+});
---
+++ Lib/__test__/engineer.test.js
@@ -0,0 +1,17 @@
+
+const Engineer = require("../engineer");
+
+test("set github account", function (){
+    const testValue = "GitHubUser";
+    const eng = new Engineer('Steve',1,"myst@test.com", testValue, "Engineer");
+    expect(eng.github).toBe(testValue);
+});
+test("getRole() should return \"Engineer\"", function (){
+    const testValue = "Engineer";
+    const eng = new Engineer("Steve",1,"myst@test.com","gitHubUser","Engineer");
+    expect(eng.getRole()).toBe(testValue);
+});
+test("Can get GitHub username W/ command", function () {
+    const testValue = "GitHubUser";
+    const eng = new Engineer("Steve",1,"myst@test.com", testValue, "Engineer");
+});
