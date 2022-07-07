1cc9b7b66d0c44416fa7d62620dc6888618793a7
macm <mac.mitsch@comcast.net>
2022-07-07 00:55:16 -0500

    working on index.js run app section

--- index.js
+++ index.js
@@ -1,65 +1,39 @@
 const inquirer = require("inquirer");
-
-const Employee = require("./__test__/Lib/employee");
-const Engineer = require("./__test__/Lib/engineer");
-const Intern = require("./__test__/Lib/intern");
-const Manager = require("./__test__/Lib/manager");
 const path = require("path");
+
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
+
 const fs = require("fs");
+const generateTeam = require("./src/template");
+
+teamArray=[];
+
+function RunApp () {
+    function createTeam () {
+        inquirer.prompt([{
+            type:"list",
+            name:"addEmployee",
+            message:"what position are adding to our team?",
+            choices: ["Engineer","Intern","Manager"]
+        }]).then(function (userInput){
+            switch(userInput.addEmployee) {
+                case "Engineer":
+                    addEngineer(); 
+                    break;
+                case "Intern":
+                    addIntern();
+                    break;
+                case "Manager":
+                    addManager();
+                    break;
+                default:
+                    htmlBuilder();
 
 
-function runInquirer() {
-    const promptArray = [{
-        type:"input",
-        name: "name",
-        message:"What is your name?"
-    },
-    {
-        type:"input",
-        name:"id",
-        message:"what is your ID?"
-    },
-    {
-        type:"input",
-        name:"email",
-        message:"What is a good Email?"
-    },
-    {
-        type:"list",
-        name:"title",
-        message:"What is your title?",
-        choices: ["Engineer", "Intern", "Manager"]
-    }];
-    return inquirer
-        .prompt(promptArray);
-}
-function runInquirerEngineer(){
-    const promptArray = [{
-        type:"input",
-        name:"github",
-        message:"What is your github?"
-    }];
-    return inquirer
-        .prompt(promptArray);
-}
-
-function runInquirerIntern(){
-    const promptArray = [{
-        type:"input",
-        name:"school",
-        message:"What school do you attend?"
-    }];
-    return inquirer
-        .prompt(promptArray);
-}
-
-function runInquirerManager() {
-    const promptArray = [{
-        type: "input",
-        name:"EmployeeNumber",
-        message: "What is your employee number?"
-    }];
-    return inquirer
-        .prompt(promptArray);
+            }
+        })
+    }
 }
 
