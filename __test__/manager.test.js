c9f5766a17b1b267ad3bf0a0ccd3d22bd2fa8224
macm <mac.mitsch@comcast.net>
2022-07-07 00:21:00 -0500

    got manager test branch working along with manager.js

--- __test__/manager.test.js
+++ __test__/manager.test.js
@@ -1,8 +1,19 @@
-const Manager = require("../lib/manager");
-
+const Manager = require("../lib/Manager");
 
 test("set office number", function () {
-    const OfficeNumber = 2;
-    const mac = new Manager("Tony",2,"test@testin.com",OfficeNumber);
-    expect(mac.OfficeNumber).toBe(OfficeNumber)
+    const testOfficeNumber = 2;
+    const mac = new Manager("James", 2, "jamesljenks@gmail.com", testOfficeNumber);
+    expect(mac.officeNumber).toBe(testOfficeNumber);
+});
+
+test("officeNumber will return office number", function() {
+    const testOfficeNumber = 2;
+    const mac= new Manager("James", 2, "jamesljenks@gmail.com", testOfficeNumber);
+    expect(mac.getOfficeNumber()).toBe(testOfficeNumber);
+});
+
+test("Test role.", function () {
+    const returnValue = "Manager";
+    const mac = new Manager("James", 2, "jamesljenks@gmail.com", 2);
+    expect(mac.getRole()).toBe(returnValue);
 });
